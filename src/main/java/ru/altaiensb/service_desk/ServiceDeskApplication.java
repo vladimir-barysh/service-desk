3d5c66604aafc129ca2309940225163d6c7df801
Vladimir Baryshnikov <megabariga2015@gmail.com>
2026-01-06 16:27:42 +0700

    1

---
+++ src/main/java/ru/altaiensb/service_desk/ServiceDeskApplication.java
@@ -0,0 +1,13 @@
+package ru.altaiensb.service_desk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceDeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceDeskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/altaiensb/service_desk/ServiceDeskApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.altaiensb.service_desk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
