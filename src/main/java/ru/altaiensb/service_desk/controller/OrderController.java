bd1a97b7ea23c25ef6f046f80817f39c487bca92
Naviko05 <Naviko05@mail.ru>
2026-02-23 23:57:20 +0700

    feat: Кнопка принять в работу + добавлен метод updateStatus

--- src/main/java/ru/altaiensb/service_desk/controller/OrderController.java
+++ src/main/java/ru/altaiensb/service_desk/controller/OrderController.java
@@ -3,6 +3,8 @@ package ru.altaiensb.service_desk.controller;
 import ru.altaiensb.service_desk.model.Order;
 import ru.altaiensb.service_desk.service.OrderService;
 import ru.altaiensb.service_desk.dto.OrderDTO;
+import ru.altaiensb.service_desk.dto.OrderStatusUpdateDTO;
+import ru.altaiensb.service_desk.model.OrderState;
 
 import lombok.RequiredArgsConstructor;
 
@@ -25,7 +27,7 @@ public class OrderController {
         return service.getAll();
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/{id}")  
     public ResponseEntity<Order> getById(@PathVariable Integer id) {
         return ResponseEntity.ok(service.getById(id));
     }
@@ -52,5 +54,10 @@ public class OrderController {
     public ResponseEntity<Void> delete(@PathVariable("id") Integer id) {
         service.delete(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @PatchMapping("/{id}/status")
+    public ResponseEntity<Order> updateStatus(@PathVariable Integer id, @RequestBody OrderStatusUpdateDTO statusDto) {
+        return ResponseEntity.ok(service.updateStatus(id, statusDto.getIdOrderState()));
     }
 }
